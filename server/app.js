657944882ebd2f80b303c6c68661632459c951bb
Sanskar <sanskaragarwal.500@gmail.com>
2018-12-19 23:13:37 +0530

    First Commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+var app = express();
+
+const publicPath = path.join(__dirname,"../public");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(PORT,() => {
+    console.log(`Server running on PORT ${PORT}`);
+});
